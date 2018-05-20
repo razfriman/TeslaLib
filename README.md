cccc77998a67fd1d26cc35626cc017d7454938a8
Raz Friman <raz.friman@razfriman.com>
2018-05-20 11:12:32 +1000

    Code refactor with Rider IDE

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ TeslaLib
 ========
 
 Tesla Model S C# API Implementation
+
+# Tesla.NET
+          
+ +[![NuGet Version](https://img.shields.io/nuget/v/TeslaLib.svg)](https://www.nuget.org/packages/TeslaLib "NuGet Version")
+ +[![NuGet Downloads](https://img.shields.io/nuget/dt/TeslaLib.svg)](https://www.nuget.org/packages/TeslaLib "NuGet Downloads")
+ +[![Latest release](https://img.shields.io/github/release/razfriman/TeslaLib.svg)](https://github.com/razfriman/TeslaLib/releases "Latest release")
--- TeslaLib.Console/Program.cs
+++ TeslaLib.Console/Program.cs
@@ -1,26 +1,45 @@
-﻿namespace TeslaLib.Console
+﻿using System.Threading.Tasks;
+
+namespace TeslaLib.Console
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            args = new string[]
+            {
+                "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384",
+                "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3",
+                "Shlomo@in-com.com",
+                "Sfriman001"
+            };
+
             if (args.Length < 4)
             {
                 System.Console.WriteLine("Not enough args. <clientId> <clientSecret> <email> <password>");
+                return;
             }
+
             var clientId = args[0];
             var clientSecret = args[1];
             var email = args[2];
             var password = args[3];
 
             var client = new TeslaClient(email, clientId, clientSecret);
-            client.Login(password);
+            await client.Login(password).ConfigureAwait(false);
 
-            var vehicles = client.LoadVehicles();
+            var vehiclesResponse = await client.LoadVehicles().ConfigureAwait(false);
 
-            foreach (TeslaVehicle car in vehicles)
+            if (vehiclesResponse.IsSuccess)
             {
-                System.Console.WriteLine(car.Id + " " + car.VIN);
+                foreach (var car in vehiclesResponse.Data)
+                {
+                    System.Console.WriteLine(car.Id + " " + car.Vin);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine($"Error loading vehicles: {vehiclesResponse.StatusCode}-{vehiclesResponse.ErrorMessage}");
             }
 
             System.Console.ReadKey();
